4d146cc1ba5ee9a516e38fde434bf64b487c5c5d
Daro-F <darochaflorian@gmail.com>
2025-07-31 16:03:20 +0200

    Article a la une

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ArticleRepository;
 
 final class AppController extends AbstractController
 {
-    #[Route('/', name: 'app')]
-    public function index(): Response
+    #[Route('/', name: 'app_')]
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAll();
+
         return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
+        'articles' => $articles
         ]);
     }
 
